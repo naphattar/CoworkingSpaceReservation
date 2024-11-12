36b484aa6c6e1c3171a3b6ea68e65e9d97b881f8
Naphat Chalermwai <naphatchalermwai@gmail.com>
2024-11-13 02:27:40 +0700

    Update CoworkingspaceCatalog.tsx

--- src/app/(coworkinginfo)/coworkingspace/components/CoworkingspaceCatalog.tsx
+++ src/app/(coworkinginfo)/coworkingspace/components/CoworkingspaceCatalog.tsx
@@ -1,20 +1,21 @@
 import Link from "next/link";
-import styles from '@/app/page.module.css';
 import Card from "@/components/Card/Card";
 import { mockCoworkingSpace } from "@/constants/constant";
 
-
-export default async function CoworkingspaceCatalog(){
+export default function CoworkingspaceCatalog() {
     return (
-        <div className={styles.cardcontainer}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
             {mockCoworkingSpace.map((coworkingspace) => (
-                <Link href={`/coworkingspace/${coworkingspace.id}`} className="w-1/5" >
+                <Link
+                    href={`/coworkingspace/${coworkingspace.id}`}
+                    key={coworkingspace.id}
+                    className="transform transition-transform duration-300 hover:scale-105"
+                >
                     <Card
-                        key={coworkingspace.id}
                         hid={coworkingspace.id}
                         coworkingspaceName={coworkingspace.name}
                         imgSrc={coworkingspace.picture}
-                        />
+                    />
                 </Link>
             ))}
         </div>
