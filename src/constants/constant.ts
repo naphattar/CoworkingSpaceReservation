b07a6e922327ccd8c663ee3e91ed244d2b587d66
Naphat Chalermwai <naphatchalermwai@gmail.com>
2024-11-13 02:17:13 +0700

    [FIX] add id to Coworkingspace interface

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,2 +1,54 @@
 export const API_URL = 'http:localhost:5000/api/v1/'
 export const API_DELEY_TIME = 500
+export const mockCoworkingSpace : CoWorkingSpace[]=  [
+    {
+      id: "1",
+      name: "Captain Buriram Coworking Space",
+      address: "123 Moo 5, Buriram Road, Muang",
+      operatingHours: "08:00 - 20:00",
+      province: "Buriram",
+      postalcode: "31000",
+      tel: "044-123-456",
+      picture: "/img/logo.jpg",
+    },
+    {
+      id: "2",
+      name: "Chiang Mai Creators Hub",
+      address: "456 Nimmanhaemin Road, Suthep",
+      operatingHours: "09:00 - 21:00",
+      province: "Chiang Mai",
+      postalcode: "50200",
+      tel: "053-456-789",
+      picture: "/img/logo.jpg",
+    },
+    {
+      id: "3",
+      name: "Bangkok Innovation Lab",
+      address: "789 Sukhumvit Road, Wattana",
+      operatingHours: "07:30 - 19:30",
+      province: "Bangkok",
+      postalcode: "10110",
+      tel: "02-789-0123",
+      picture: "/img/logo.jpg",
+    },
+    {
+      id: "4",
+      name: "Phuket Workspace",
+      address: "321 Patong Beach Road, Kathu",
+      operatingHours: "08:00 - 22:00",
+      province: "Phuket",
+      postalcode: "83150",
+      tel: "076-234-567",
+      picture: "/img/logo.jpg",
+    },
+    {
+      id: "5",
+      name: "Khon Kaen CoLab",
+      address: "102 Rop Bueng Road, Nai Mueang",
+      operatingHours: "08:30 - 18:00",
+      province: "Khon Kaen",
+      postalcode: "40000",
+      tel: "043-321-654",
+      picture: "/img/logo.jpg",
+    }
+  ];
