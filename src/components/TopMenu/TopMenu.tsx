a52b2a2f6643305477f08afebfc883cff4ec15fd
Naphat Chalermwai <naphatchalermwai@gmail.com>
2024-11-13 01:57:06 +0700

    [Update] add app name

--- src/components/TopMenu/TopMenu.tsx
+++ src/components/TopMenu/TopMenu.tsx
@@ -3,14 +3,22 @@ import TopMenuItem from "./TopMenuItem";
 
 export default function TopMenu() {
     return (
-        <div className="fixed top-0 left-0 right-0 z-30 flex justify-end h-16 p-4 pr-10 items-center bg-white shadow-md space-x-8">
-            <TopMenuItem title={"Booking"} pageRef={"/booking"}/>
-            <Image 
-                src={"/img/logo.jpg"} 
-                alt={"logo"}
-                width={30}
-                height={30}
-            />
+        <div className="fixed top-0 left-0 right-0 z-30 flex justify-between h-16 p-4 items-center bg-white shadow-md space-x-8">
+            {/* Left Side: Title Text */}
+            <div className="text-lg font-semibold whitespace-nowrap truncate p-6">
+                Captain Buriram
+            </div>
+
+            {/* Right Side: Menu Items and Logo */}
+            <div className="flex items-center space-x-8">
+                <TopMenuItem title={"Booking"} pageRef={"/booking"} />
+                <Image 
+                    src={"/img/logo.jpg"} 
+                    alt={"logo"}
+                    width={30}
+                    height={30}
+                />
+            </div>
         </div>
     );
 }
