28212f4d24e52355773c69fb8324519426e57c50
Naphat Chalermwai <naphatchalermwai@gmail.com>
2024-11-13 02:17:33 +0700

    [ADD] add coworkingspace list page

--- src/components/TopMenu/TopMenu.tsx
+++ src/components/TopMenu/TopMenu.tsx
@@ -3,7 +3,7 @@ import TopMenuItem from "./TopMenuItem";
 
 export default function TopMenu() {
     return (
-        <div className="fixed top-0 left-0 right-0 z-30 flex justify-between h-16 p-4 items-center bg-white shadow-md space-x-8">
+        <div className="fixed top-0 left-0 right-0 z-30 flex justify-between h-16 p-4 pr-10 items-center bg-white shadow-md space-x-8">
             {/* Left Side: Title Text */}
             <div className="text-lg font-semibold whitespace-nowrap truncate p-6">
                 Captain Buriram
