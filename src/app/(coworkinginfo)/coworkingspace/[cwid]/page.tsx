47bdfad1d1fe95bf48c8a1df76a4b66335c00d10
Naphat Chalermwai <naphatchalermwai@gmail.com>
2024-11-13 22:53:54 +0700

    [Update] update coworkingspace id page

--- src/app/(coworkinginfo)/coworkingspace/[cwid]/page.tsx
+++ src/app/(coworkinginfo)/coworkingspace/[cwid]/page.tsx
@@ -1,19 +1,20 @@
 import { mockCoworkingSpace } from "@/constants/constant";
+import getCoworkingspace from "@/libs/Coworkingspace/getCoworkingspace";
 import Image from "next/image";
 
 export default async function hospitalDetailPage({ params }: { params: { cwid: string } }) {
-    const coworkingspace = mockCoworkingSpace[parseInt(params.cwid) - 1];
+    const coworkingspace = await getCoworkingspace(params.cwid)
 
     return (
         <main className="p-6">
             {/* Title */}
-            <h1 className="text-2xl font-bold text-gray-800 mb-4 text-center">{coworkingspace.name}</h1>
+            <h1 className="text-2xl font-bold text-gray-800 mb-4 text-center">{coworkingspace.data.name}</h1>
 
             {/* Details Section */}
             <div className="flex flex-col md:flex-row items-center md:items-start md:space-x-8 bg-white shadow-lg rounded-lg p-6 md:p-8">
                 {/* Image */}
                 <Image
-                    src={coworkingspace.picture}
+                    src={coworkingspace.data.picture}
                     alt={`coworking space ${params.cwid}`}
                     width={0}
                     height={0}
@@ -24,19 +25,19 @@ export default async function hospitalDetailPage({ params }: { params: { cwid: s
                 {/* Information */}
                 <div className="mt-4 md:mt-0 w-full md:w-2/3 text-left space-y-3">
                     <p className="text-lg">
-                        <span className="font-semibold text-gray-700">Address:</span> {coworkingspace.address}
+                        <span className="font-semibold text-gray-700">Address:</span> {coworkingspace.data.address}
                     </p>
                     <p className="text-lg">
-                        <span className="font-semibold text-gray-700">Operating Hours:</span> {coworkingspace.operatingHours}
+                        <span className="font-semibold text-gray-700">Operating Hours:</span> {coworkingspace.data.operatingHours}
                     </p>
                     <p className="text-lg">
-                        <span className="font-semibold text-gray-700">Province:</span> {coworkingspace.province}
+                        <span className="font-semibold text-gray-700">Province:</span> {coworkingspace.data.province}
                     </p>
                     <p className="text-lg">
-                        <span className="font-semibold text-gray-700">Postal Code:</span> {coworkingspace.postalcode}
+                        <span className="font-semibold text-gray-700">Postal Code:</span> {coworkingspace.data.postalcode}
                     </p>
                     <p className="text-lg">
-                        <span className="font-semibold text-gray-700">Telephone:</span> {coworkingspace.tel}
+                        <span className="font-semibold text-gray-700">Telephone:</span> {coworkingspace.data.tel}
                     </p>
                 </div>
             </div>
