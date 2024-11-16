821f2ed104a09a8da42ed43a1cf04d88120556b1
supakorn22 <6432173821@student.chula.ac.th>
2024-11-16 23:52:39 +0700

    feat: :sparkles: add booking function in lib.

--- src/interfaces/Booking.ts
+++ src/interfaces/Booking.ts
@@ -1,7 +1,40 @@
-interface Booking{
-    bookingDate : string,
-    numOfRooms : number,
-    user : string,
-    coworkingspace : string,
-    createdAt : string
+
+
+
+interface CreateBookingRequest {
+    bookingDate: string;
+    numOfRooms: number;
+    createdAt: string;
+  }
+  
+
+interface Booking {
+    _id?: string;
+    bookingDate: string; // Use `Date` instead of `string` if you parse this into a `Date` object
+    numOfRooms: number;
+    user: string;
+    coworkingspace: CoWorkingSpace;
+    createdAt: string; // Same as above, replace with `Date` if parsed
+    __v?: number;
+}
+
+interface getBookingsResponse {
+    success: boolean;
+    count: number;
+    data: Booking[];
+}
+
+interface createBookingResponse {
+    success: boolean;
+    data: Booking | string;
+}
+
+interface getBookingResponse {
+    success: boolean;
+    data: Booking;
+}
+
+interface deleteBookingResponse {
+    success: boolean;
+    data: {};
 }
